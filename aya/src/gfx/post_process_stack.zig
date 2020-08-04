e55b075acccdf1986bb91c02e7f8eaf42f7fb82b
Mike <mike@prime31.com>
2020-08-03 23:07:32 -0700

    batcher works now

--- aya/src/gfx/post_process_stack.zig
+++ aya/src/gfx/post_process_stack.zig
@@ -1,17 +1,14 @@
 const std = @import("std");
 const aya = @import("../aya.zig");
-const RenderTexture = aya.gfx.RenderTexture;
 
 pub const PostProcessStack = struct {
     processors: std.ArrayList(*PostProcessor),
-    render_texture: RenderTexture,
     allocator: *std.mem.Allocator,
 
     pub fn init(allocator: ?*std.mem.Allocator, design_w: i32, design_h: i32) PostProcessStack {
         const alloc = allocator orelse aya.mem.allocator;
         return .{
             .processors = std.ArrayList(*PostProcessor).init(alloc),
-            .render_texture = RenderTexture.init(design_w, design_h),
             .allocator = alloc,
         };
     }
@@ -21,10 +18,9 @@ pub const PostProcessStack = struct {
             p.deinit(p, self.allocator);
         }
         self.processors.deinit();
-        self.render_texture.deinit();
     }
 
-    pub fn add(self: *PostProcessStack, comptime T: type, data: var) *T {
+    pub fn add(self: *PostProcessStack, comptime T: type, data: anytype) *T {
         std.debug.assert(@hasDecl(T, "initialize"));
         std.debug.assert(@hasDecl(T, "deinit"));
         std.debug.assert(@hasField(T, "postprocessor"));
@@ -44,25 +40,15 @@ pub const PostProcessStack = struct {
         return processor;
     }
 
-    pub fn process(self: *PostProcessStack, rt: *RenderTexture) void {
-        std.debug.assert(rt.tex.width == self.render_texture.tex.width);
-        std.debug.assert(rt.tex.height == self.render_texture.tex.height);
-
-        for (self.processors.items) |p, i| {
-            const src = if (aya.math.isEven(i)) rt.* else self.render_texture;
-            const dst = if (!aya.math.isEven(i)) rt.* else self.render_texture;
-            p.process(p, src, dst);
-        }
-
-        // if there was an odd number of post processors swap the RTs so the last one written to is what gets blitted later
-        if (!aya.math.isEven(self.processors.items.len)) {
-            std.mem.swap(RenderTexture, rt, &self.render_texture);
+    pub fn process(self: *PostProcessStack, pass: aya.gfx.OffscreenPass) void {
+        for (self.processors.items) |p| {
+            p.process(p, pass.color_tex);
         }
     }
 };
 
 pub const PostProcessor = struct {
-    process: fn (*PostProcessor, RenderTexture, RenderTexture) void,
+    process: fn (*PostProcessor, aya.gfx.Texture) void,
     deinit: fn (self: *PostProcessor, allocator: *std.mem.Allocator) void = undefined,
 
     pub fn getParent(self: *PostProcessor, comptime T: type) *T {
@@ -71,29 +57,29 @@ pub const PostProcessor = struct {
 
     // helper method for taking the final texture from a postprocessor and blitting it. Simple postprocessors
     // can get away with just calling this method directly.
-    pub fn blit(self: PostProcessor, src: RenderTexture, dst: RenderTexture, shader: aya.gfx.Shader) void {
-        aya.gfx.beginPass(.{ .render_texture = dst, .shader = shader });
-        aya.draw.tex(src.tex, 0, 0);
+    pub fn blit(self: PostProcessor, tex: aya.gfx.Texture, pipeline: aya.gfx.Pipeline) void {
+        aya.gfx.beginPass(.{ .color_action = .SG_ACTION_DONTCARE, .pipeline = pipeline });
+        aya.draw.tex(tex, 0, 0);
         aya.gfx.endPass();
     }
 };
 
 pub const Sepia = struct {
     postprocessor: PostProcessor,
-    shader: aya.gfx.Shader,
+    pipeline: aya.gfx.Pipeline,
 
     pub fn deinit(self: @This()) void {
-        self.shader.deinit();
+        self.pipeline.deinit();
     }
 
-    pub fn initialize(self: *@This(), data: var) void {
-        self.shader = aya.gfx.Shader.initFromFile("assets/Sepia.fxb") catch unreachable;
-        self.shader.setParam(aya.math.Vec3, "_sepiaTone", aya.math.Vec3{ .x = 1.2, .y = 1.0, .z = 0.8 });
+    pub fn initialize(self: *@This(), data: anytype) void {
+        self.pipeline = aya.gfx.Pipeline.makeDefaultPipeline();
+        // self.shader.setParam(aya.math.Vec3, "_sepiaTone", aya.math.Vec3{ .x = 1.2, .y = 1.0, .z = 0.8 });
         self.postprocessor = .{ .process = process };
     }
 
-    pub fn process(processor: *PostProcessor, src: RenderTexture, dst: RenderTexture) void {
+    pub fn process(processor: *PostProcessor, tex: aya.gfx.Texture) void {
         const self = processor.getParent(@This());
-        processor.blit(src, dst, self.shader);
+        processor.blit(tex, self.pipeline);
     }
 };
--- examples/offscreen.zig
+++ examples/offscreen.zig
@@ -5,7 +5,7 @@ const math = aya.math;
 
 var checker_tex: aya.gfx.Texture = undefined;
 var font_tex: aya.gfx.Texture = undefined;
-var rt: aya.gfx.RenderTexture = undefined;
+var pass: aya.gfx.OffscreenPass = undefined;
 var stack: aya.gfx.PostProcessStack = undefined;
 
 pub fn main() !void {
@@ -13,45 +13,48 @@ pub fn main() !void {
         .init = init,
         .update = update,
         .render = render,
+        .shutdown = shutdown,
         .gfx = .{
             .resolution_policy = .show_all_pixel_perfect,
-        }
+        },
     });
-
-    checker_tex.deinit();
-    font_tex.deinit();
-    rt.deinit();
-    stack.deinit();
 }
 
 fn init() void {
     checker_tex = aya.gfx.Texture.initCheckerboard();
-    font_tex = aya.gfx.Texture.initFromFile("assets/font.png") catch unreachable;
-    rt = aya.gfx.RenderTexture.init(52, 52);
+    font_tex = aya.gfx.Texture.initFromFile("assets/font.png", .linear) catch unreachable;
+    pass = aya.gfx.OffscreenPass.init(52, 52, .nearest);
 
     stack = aya.gfx.createPostProcessStack();
     _ = stack.add(aya.gfx.Sepia, {});
 }
 
-fn update() void {
-    if (aya.input.keyUp(.SDL_SCANCODE_K)) {
-        var w: i32 = 0;
-        var h: i32 = 0;
-        aya.window.size(&w, &h);
+fn shutdown() void {
+    checker_tex.deinit();
+    font_tex.deinit();
+    pass.deinit();
+    stack.deinit();
+}
 
-        if (w < 1280) {
-            aya.window.setSize(1280, 960);
-        } else {
-            aya.window.setSize(640, 480);
-        }
-    }
+fn update() void {
+    // if (aya.input.keyUp(.SDL_SCANCODE_K)) {
+    //     var w: i32 = 0;
+    //     var h: i32 = 0;
+    //     aya.window.size(&w, &h);
+
+    //     if (w < 1280) {
+    //         aya.window.setSize(1280, 960);
+    //     } else {
+    //         aya.window.setSize(640, 480);
+    //     }
+    // }
 }
 
 fn render() void {
     // render offscreen
-    aya.gfx.beginPass(.{.render_texture = rt, .color = aya.math.Color.lime});
+    aya.gfx.beginPass(.{ .pass = pass, .color = aya.math.Color.lime });
     var i = @as(usize, 0);
-    while (i <= @divFloor(rt.tex.width, checker_tex.width)) : (i += 1) {
+    while (i <= @divFloor(pass.color_tex.width, checker_tex.width)) : (i += 1) {
         aya.draw.tex(checker_tex, @intToFloat(f32, i * 4), @intToFloat(f32, i * 2 + 10));
     }
     aya.gfx.endPass();
@@ -60,16 +63,18 @@ fn render() void {
     aya.gfx.beginPass(.{});
     aya.draw.texScale(checker_tex, 5, 5, 10);
     aya.draw.texScale(checker_tex, 55, 55, 2);
+    // aya.gfx.flush();
 
     aya.draw.texScale(font_tex, 200, 100, 0.2);
     aya.draw.texScale(font_tex, 300, 200, 0.2);
+    // aya.gfx.flush();
 
-    aya.draw.line(aya.math.Vec2.init(0, 0), aya.math.Vec2.init(640, 480), 2, aya.math.Color.blue);
-    aya.draw.point(math.Vec2.init(350, 350), 10, math.Color.sky_blue);
-    aya.draw.point(math.Vec2.init(380, 380), 15, math.Color.magenta);
-    aya.draw.rect(math.Vec2.init(387, 372), 40, 15, math.Color.dark_brown);
-    aya.draw.hollowRect(math.Vec2.init(430, 372), 40, 15, 2, math.Color.yellow);
-    aya.draw.circle(math.Vec2.init(100, 350), 20, 1, 12, math.Color.orange);
+    aya.draw.line(.{ .x = 0, .y = 0 }, aya.math.Vec2.init(640, 480), 2, aya.math.Color.blue);
+    aya.draw.point(.{ .x = 350, .y = 350 }, 10, math.Color.sky_blue);
+    aya.draw.point(.{ .x = 380, .y = 380 }, 15, math.Color.magenta);
+    aya.draw.rect(.{ .x = 387, .y = 372 }, 40, 15, math.Color.dark_brown);
+    aya.draw.hollowRect(.{ .x = 430, .y = 372 }, 40, 15, 2, math.Color.yellow);
+    aya.draw.circle(.{ .x = 100, .y = 350 }, 20, 1, 12, math.Color.orange);
 
     const poly = [_]math.Vec2{ .{ .x = 400, .y = 30 }, .{ .x = 420, .y = 10 }, .{ .x = 430, .y = 80 }, .{ .x = 410, .y = 60 }, .{ .x = 375, .y = 40 } };
     aya.draw.hollowPolygon(poly[0..], 2, math.Color.gold);
@@ -80,8 +85,8 @@ fn render() void {
     aya.gfx.blitToScreen(aya.math.Color.black);
 
     // now render directly to the backbuffer
-    aya.gfx.beginPass(.{});
-    aya.debug.drawPoint(math.Vec2.init(30, 400), 60, aya.math.Color.yellow);
-    aya.draw.texScale(rt.tex, 0, 200, 2);
+    aya.gfx.beginPass(.{ .color_action = .SG_ACTION_DONTCARE });
+    aya.debug.drawPoint(.{ .x = 30, .y = 400 }, 60, aya.math.Color.yellow);
+    aya.draw.texScale(pass.color_tex, 0, 200, 2);
     aya.gfx.endPass();
 }
