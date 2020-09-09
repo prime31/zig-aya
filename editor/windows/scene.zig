8464f03cdb75e3b5540251f89725591c73080d78
Mike <mike@prime31.com>
2020-09-09 11:30:53 -0700

    grid lines

--- editor/windows/scene.zig
+++ editor/windows/scene.zig
@@ -36,9 +36,18 @@ const Camera = struct {
         return inv_trans_mat.transformVec2(pos);
     }
 
+    /// calls screenToWorld converting to ImVec2s
     pub fn igScreenToWorld(self: Camera, pos: ImVec2) ImVec2 {
         const tmp = self.screenToWorld(math.Vec2{ .x = pos.x, .y = pos.y });
         return .{ .x = tmp.x, .y = tmp.y };
+    }
+
+    pub fn bounds(self: Camera) math.Rect {
+        var window_size = ogGetWindowSize();
+        var tl = self.screenToWorld(.{});
+        var br = self.screenToWorld(math.Vec2{ .x = window_size.x, .y = window_size.y });
+
+        return math.Rect{ .x = tl.x, .y = tl.y, .w = br.x - tl.x, .h = br.y - tl.y };
     }
 };
 
@@ -81,8 +90,43 @@ pub const Scene = struct {
         // get mouse in world space
         const mouse = self.cam.igScreenToWorld(igGetIO().MousePos.subtract(ogGetCursorScreenPos()));
 
+        self.drawGridLines();
+
         aya.draw.rect(.{}, 40, 40, math.Color.white);
         aya.draw.text("wtf", mouse.x, mouse.y, null);
+    }
+
+    fn drawGridLines(self: @This()) void {
+        var grid_major_size: usize = 16;
+        var grid_major_color: math.Color = math.Color.fromBytes(255, 255, 255, 50);
+        const bounds = self.cam.bounds();
+
+        // get our first visible grid lines
+        const grid_size = @intCast(i32, grid_major_size);
+        const grid_size_cutoff = grid_size - 1;
+        const first_grid_x = @divTrunc(@floatToInt(i32, bounds.x) - grid_size_cutoff, grid_size_cutoff) * grid_size;
+        const first_grid_y = @divTrunc(@floatToInt(i32, bounds.y) - grid_size_cutoff, grid_size_cutoff) * grid_size;
+
+        // TODO: when zoomed out far enough, double the grid lines and only display every other one
+        var x = first_grid_x;
+        var y = first_grid_y;
+        while (@intToFloat(f32, y) < bounds.h) : (y += grid_size) {
+            while (@intToFloat(f32, x) < bounds.w) : (x += grid_size) {
+                var top = math.Vec2{ .x = @intToFloat(f32, x), .y = @intToFloat(f32, y) };
+                var bottom = top.add(.{ .y = bounds.h + @intToFloat(f32, grid_size) });
+                aya.draw.line(top, bottom, 1, grid_major_color);
+            }
+        }
+
+        x = first_grid_x;
+        y = first_grid_y;
+        while (@intToFloat(f32, x) < bounds.w) : (x += grid_size) {
+            while (@intToFloat(f32, y) < bounds.h) : (y += grid_size) {
+                var left = math.Vec2{ .x = @intToFloat(f32, x), .y = @intToFloat(f32, y) };
+                var right = left.add(.{ .x = bounds.w + @intToFloat(f32, grid_size) });
+                aya.draw.line(left, right, 1, grid_major_color);
+            }
+        }
     }
 
     fn handleInput(self: *@This()) void {
