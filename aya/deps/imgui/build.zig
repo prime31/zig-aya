da9cd4148e919d48c91bc5871e67f72b16e6a1b7
Mike <mike@prime31.com>
2020-11-13 11:34:36 -0800

    windows builds are uncursed

--- aya/deps/imgui/build.zig
+++ aya/deps/imgui/build.zig
@@ -60,14 +60,21 @@ fn addImGuiGlImplementation(b: *Builder, exe: *std.build.LibExeObjStep, target: 
         const lib = b.addStaticLibrary("gl3w", null);
         lib.setBuildMode(b.standardReleaseOptions());
         lib.setTarget(target);
+        lib.linkLibC();
 
-        if (target.isDarwin()) {
+        if (target.isWindows()) {
+            lib.linkSystemLibrary("user32");
+            lib.linkSystemLibrary("gdi32");
+        } else if (target.isDarwin()) {
             const frameworks_dir = macosFrameworksDir(b) catch unreachable;
             lib.addFrameworkDir(frameworks_dir);
             // for some reason, only on some SDL installs this is required...
             // const x11_include_dir = std.mem.concat(b.allocator, u8, &[_][]const u8{ frameworks_dir, "/Tk.framework/Headers" }) catch unreachable;
             // lib.addIncludeDir(x11_include_dir);
+        } else {
+            lib.linkSystemLibrary("c++");
         }
+
         lib.addIncludeDir(base_path ++ "cimgui/imgui");
         lib.addIncludeDir(base_path ++ "cimgui/imgui/examples/libs/gl3w");
         lib.addIncludeDir("/usr/local/include/SDL2");
