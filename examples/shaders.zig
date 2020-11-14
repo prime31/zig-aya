6072c67fb413a2d46b8793d94949b2668312b57b
Mike <mike@prime31.com>
2020-11-13 16:51:31 -0800

    post process stack works

--- examples/shaders.zig
+++ examples/shaders.zig
@@ -14,31 +14,34 @@ var noise_shader: Shader = undefined;
 var dissolve_shader: Shader = undefined;
 var stack: aya.gfx.PostProcessStack = undefined;
 
-var noise = aya.gfx.effects.Noise.Params{
-    .time = 0,
-    .power = 100,
-};
+var lines = struct {
+    line_size: f32 = 4,
+    line_color: aya.math.Vec4 = .{ .x = 0.9, .y = 0.8, .z = 0.6, .w = 1.0 },
+}{};
 
-var lines = aya.gfx.effects.Lines.Params{
-    .line_size = 4,
-    .line_color = .{ .x = 0.9, .y = 0.8, .z = 0.6, .w = 1.0 },
-};
+var noise = struct {
+    power: f32 = 100,
+}{};
 
-var dissolve = aya.gfx.effects.Dissolve.Params{
-    .progress = 0,
-    .threshold = 0.04,
-    .threshold_color = aya.math.Color.orange.asVec4(),
-};
+var dissolve = struct {
+    threshold: f32 = 0.04,
+    threshold_color: aya.math.Vec4 = aya.math.Color.orange.asVec4(),
+}{};
 
-var glitch = aya.gfx.PixelGlitch.Params{
-    .vertical_size = 1,
-    .horizontal_offset = 1,
-    .screen_size = .{},
-};
+var sepia = struct {
+    sepia_tone: aya.math.Vec3 = .{ .x = 1.2, .y = 1.0, .z = 0.8 },
+}{};
+
+var vignette_param = struct {
+    radius: f32 = 1.25,
+    power: f32 = 1,
+}{};
+
+var glitch = aya.gfx.PixelGlitch.Params{};
 
 const noise_frag: [:0]const u8 =
     \\uniform float time;
-    \\uniform float power;
+    \\uniform float power = 100;
     \\
     \\float rand(vec2 co){
     \\  return fract(sin(dot(co.xy, vec2(12.9898, 78.233))) * 43758.5453);
@@ -54,12 +57,11 @@ const noise_frag: [:0]const u8 =
 ;
 
 const lines_frag: [:0]const u8 =
-    \\uniform float line_size;
-    \\uniform vec4 line_color;
+    \\uniform float line_size = 4.0;
+    \\uniform vec4 line_color = vec4(0.9, 0.8, 0.6, 1.0);
     \\
     \\vec4 effect(sampler2D tex, vec2 tex_coord, vec4 vert_color) {
     \\  float alpha = texture(tex, tex_coord).a;
-    \\  vec2 screen_size = vec2(1024.0, 768.0);
     \\  vec2 screen_pos = gl_FragCoord.xy;
     \\
     \\  float flooredAlternate = mod(floor(screen_pos.y / line_size), 2.0);
@@ -107,6 +109,7 @@ pub fn main() !void {
 fn init() !void {
     tex = aya.gfx.Texture.initFromFile("assets/sword_dude.png", .nearest) catch unreachable;
     clouds_tex = aya.gfx.Texture.initFromFile("assets/clouds.png", .linear) catch unreachable;
+
     lines_shader = try Shader.initWithFrag(lines_frag);
     noise_shader = try Shader.initWithFrag(noise_frag);
     dissolve_shader = try Shader.initWithFrag(dissolve_frag);
@@ -114,9 +117,9 @@ fn init() !void {
     dissolve_shader.setUniformName(i32, "dissolve_tex", 1);
 
     stack = aya.gfx.createPostProcessStack();
-    // _ = stack.add(aya.gfx.PixelGlitch, {});
-    // _ = stack.add(aya.gfx.Sepia, {});
-    // _ = stack.add(aya.gfx.Vignette, {});
+    _ = stack.add(aya.gfx.PixelGlitch, {});
+    _ = stack.add(aya.gfx.Sepia, {});
+    _ = stack.add(aya.gfx.Vignette, {});
 }
 
 fn shutdown() !void {
@@ -132,34 +135,50 @@ fn shutdown() !void {
 fn update() !void {
     _ = igCheckbox("Enable PostProcessing", &post_process);
 
-    // _ = aya.utils.inspect("glitch", &glitch);
-    // // if (@mod(aya.time.frames(), 5) == 0) {
-    // //      glitch.vertical_size = aya.math.rand.range(f32, 1, 5);
-    // //      glitch.horizontal_offset = aya.math.rand.range(f32, 1, 10);
-    // // }
-    // glitch.screen_size = aya.window.sizeVec2();
-    // stack.processors.items[0].getParent(aya.gfx.PixelGlitch).setParams(glitch);
+    if (aya.utils.inspect("lines", &lines)) {
+        lines_shader.bind();
+        lines_shader.setUniformName(f32, "line_size", lines.line_size);
+        lines_shader.setUniformName(aya.math.Vec4, "line_color", lines.line_color);
+    }
+    if (aya.utils.inspect("noise", &noise)) {
+        noise_shader.bind();
+        noise_shader.setUniformName(f32, "power", 100);
+    }
+    if (aya.utils.inspect("dissolve", &dissolve)) {
+        dissolve_shader.bind();
+        dissolve_shader.setUniformName(f32, "threshold", dissolve.threshold);
+        dissolve_shader.setUniformName(aya.math.Vec4, "threshold_color", dissolve.threshold_color);
+    }
+    if (aya.utils.inspect("pixel glitch", &glitch)) {
+        stack.processors.items[0].getParent(aya.gfx.PixelGlitch).setUniforms(glitch.vertical_size, glitch.horizontal_offset);
+    }
+    if (aya.utils.inspect("sepia", &sepia)) {
+        stack.processors.items[1].getParent(aya.gfx.Sepia).setTone(sepia.sepia_tone);
+    }
+    if (aya.utils.inspect("vignette", &vignette_param)) {
+        stack.processors.items[2].getParent(aya.gfx.Vignette).setUniforms(vignette_param.radius, vignette_param.power);
+    }
 
-    // _ = aya.utils.inspect("dissolve", &dissolve);
-    // dissolve.progress = aya.math.pingpong(aya.time.seconds(), 1);
-    // dissolve_pip.setFragUniform(0, &dissolve);
+    if (@mod(aya.time.frames(), 5) == 0) {
+         glitch.vertical_size = aya.math.rand.range(f32, 1, 5);
+         glitch.horizontal_offset = aya.math.rand.range(f32, -20, 20);
+    }
+    stack.processors.items[0].getParent(aya.gfx.PixelGlitch).setUniforms(glitch.vertical_size, glitch.horizontal_offset);
 }
 
 fn render() !void {
     aya.gfx.beginPass(.{});
-    aya.draw.text("Hold space to disable effects", 0, 30, null);
+    aya.draw.text("Hold space to disable noise effects", 0, 30, null);
+    aya.draw.text("Hold d to disable dissolve effect", 00, 50, null);
     aya.draw.texScale(tex, 30, 30, 3);
 
     if (!aya.input.keyDown(.space)) {
         aya.gfx.setShader(lines_shader);
-        lines_shader.setUniformName(f32, "line_size", 4);
-        lines_shader.setUniformName(aya.math.Vec4, "line_color", .{ .x = 1, .y = 0.8, .z = 0.6, .w = 1 });
     }
     aya.draw.texScale(tex, 230, 230, 3);
 
     if (!aya.input.keyDown(.space)) {
         aya.gfx.setShader(noise_shader);
-        noise_shader.setUniformName(f32, "power", 100);
         noise_shader.setUniformName(f32, "time", aya.time.seconds());
     }
     aya.draw.texScale(tex, 100, 230, 3);
@@ -167,10 +186,8 @@ fn render() !void {
 
     if (!aya.input.keyDown(.d)) {
         aya.gfx.setShader(dissolve_shader);
-        dissolve_shader.setUniformName(f32, "progress", aya.math.pingpong(aya.time.seconds(), 1));
-        dissolve_shader.setUniformName(f32, "threshold", 0.4);
-        dissolve_shader.setUniformName(aya.math.Vec4, "threshold_color", aya.math.Color.orange.asVec4());
         aya.draw.bindTexture(clouds_tex, 1);
+        dissolve_shader.setUniformName(f32, "progress", aya.math.pingpong(aya.time.seconds(), 1));
     }
     aya.draw.texScale(tex, 330, 30, 3);
 
