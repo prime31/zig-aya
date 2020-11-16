71b61832529935c50df9b1041c3c1579af1c7720
Mike <mike@prime31.com>
2020-11-15 21:25:57 -0800

    cleanup build

--- build.zig
+++ build.zig
@@ -13,13 +13,12 @@ pub fn build(b: *Builder) void {
 
     // first item in list will be added as "run" so `zig build run` will always work
     const examples = [_][2][]const u8{
-        // [_][]const u8{ "empty", "examples/empty.zig" },
-        // [_][]const u8{ "editor", "editor/main.zig" },
-        // [_][]const u8{ "mode7", "examples/mode7.zig" },
-        // [_][]const u8{ "markov", "examples/markov.zig" },
-        // [_][]const u8{ "clipper", "examples/clipped_sprite.zig" },
-        // [_][]const u8{ "primitives", "examples/primitives.zig" },
-        // [_][]const u8{ "entities", "examples/entities.zig" },
+        [_][]const u8{ "editor", "editor/main.zig" },
+        [_][]const u8{ "mode7", "examples/mode7.zig" },
+        [_][]const u8{ "markov", "examples/markov.zig" },
+        [_][]const u8{ "clipper", "examples/clipped_sprite.zig" },
+        [_][]const u8{ "primitives", "examples/primitives.zig" },
+        [_][]const u8{ "entities", "examples/entities.zig" },
         [_][]const u8{ "shaders", "examples/shaders.zig" },
         [_][]const u8{ "atlas_batch", "examples/atlas_batch.zig" },
         [_][]const u8{ "tilemap", "examples/tilemap.zig" },
@@ -27,16 +26,22 @@ pub fn build(b: *Builder) void {
         [_][]const u8{ "batcher", "examples/batcher.zig" },
         [_][]const u8{ "offscreen", "examples/offscreen.zig" },
         [_][]const u8{ "dynamic_mesh", "examples/dynamic_mesh.zig" },
+        [_][]const u8{ "instanced_mesh", "examples/instanced_mesh.zig" },
         [_][]const u8{ "mesh", "examples/mesh.zig" },
         [_][]const u8{ "imgui", "examples/imgui.zig" },
+        [_][]const u8{ "empty", "examples/empty.zig" },
     };
 
+    const examples_step = b.step("examples", "build all examples");
+    b.default_step.dependOn(examples_step);
+
     for (examples) |example, i| {
-        createExe(b, target, example[0], example[1]);
+        var exe = createExe(b, target, example[0], example[1]);
+        examples_step.dependOn(&exe.step);
 
         // first element in the list is added as "run" so "zig build run" works
         if (i == 0) {
-            createExe(b, target, "run", example[1]);
+            _ = createExe(b, target, "run", example[1]);
         }
     }
 
@@ -44,7 +49,7 @@ pub fn build(b: *Builder) void {
 }
 
 /// creates an exe with all the required dependencies
-fn createExe(b: *Builder, target: Target, name: []const u8, source: []const u8) void {
+fn createExe(b: *Builder, target: Target, name: []const u8, source: []const u8) *std.build.LibExeObjStep {
     var exe = b.addExecutable(name, source);
     exe.setBuildMode(b.standardReleaseOptions());
     exe.setOutputDir("zig-cache/bin");
@@ -54,6 +59,8 @@ fn createExe(b: *Builder, target: Target, name: []const u8, source: []const u8) 
     const run_cmd = exe.run();
     const exe_step = b.step(name, b.fmt("run {}.zig", .{name}));
     exe_step.dependOn(&run_cmd.step);
+
+    return exe;
 }
 
 /// adds Aya and all dependencies to artifact
@@ -64,32 +71,32 @@ pub fn addAyaToArtifact(b: *Builder, artifact: *std.build.LibExeObjStep, target:
     artifact.addBuildOption(bool, "enable_imgui", enable_imgui.?);
 
     // STB Image, Image Write, Rect Pack
-    const stb_build = @import(prefix_path ++ "aya/deps/stb/build.zig");
+    const stb_build = @import("aya/deps/stb/build.zig");
     stb_build.linkArtifact(b, artifact, target, prefix_path);
+    const stb_pkg = stb_build.getPackage(prefix_path);
 
     // FontStash
-    const fontstash_build = @import(prefix_path ++ "aya/deps/fontstash/build.zig");
+    const fontstash_build = @import("aya/deps/fontstash/build.zig");
     fontstash_build.linkArtifact(b, artifact, target, prefix_path);
+    const fontstash_pkg = fontstash_build.getPackage(prefix_path);
 
     // Dear ImGui
     // TODO: skip adding imgui altogether when enable_imgui is false. This would require builds to be made with -Denable_imgui=true
-    const imgui_build = @import(prefix_path ++ "aya/deps/imgui/build.zig");
+    const imgui_build = @import("aya/deps/imgui/build.zig");
     imgui_build.linkArtifact(b, artifact, target, prefix_path);
     const imgui_pkg = imgui_build.getImGuiPackage(prefix_path);
     const imgui_gl_pkg = imgui_build.getImGuiGlPackage(prefix_path);
 
     // RenderKit
-    const renderkit_build = @import(prefix_path ++ "aya/deps/renderkit/build.zig");
+    const renderkit_build = @import("aya/deps/renderkit/build.zig");
     renderkit_build.addRenderKitToArtifact(b, artifact, target, prefix_path ++ "aya/deps/renderkit/");
     const renderkit_pkg = renderkit_build.getRenderKitPackage(prefix_path ++ "aya/deps/renderkit/");
 
     // SDL
-    const sdl_build = @import(prefix_path ++ "aya/deps/sdl/build.zig");
+    const sdl_build = @import("aya/deps/sdl/build.zig");
     sdl_build.linkArtifact(b, artifact, target, prefix_path);
     const sdl_pkg = sdl_build.getPackage(prefix_path);
 
-    const stb_pkg = stb_build.getPackage(prefix_path);
-    const fontstash_pkg = fontstash_build.getPackage(prefix_path);
 
     const aya = Pkg{
         .name = "aya",
