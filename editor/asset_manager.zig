74e9b18e59ec1c8ab511abe7a406c9d7b43a2e9a
Mike Desaro <mike@prime31.com>
2020-12-20 14:20:32 -0800

    bug fix

--- editor/asset_manager.zig
+++ editor/asset_manager.zig
@@ -11,7 +11,7 @@ pub const AssetManager = struct {
     }
 
     pub fn deinit(self: @This()) void {
-        aya.mem.allocator.free(self.root_path);
+        if (self.root_path.len > 0) aya.mem.allocator.free(self.root_path);
     }
 
     /// sets the root project path and starts a scan of the subfolders to load up the asset state
@@ -22,7 +22,7 @@ pub const AssetManager = struct {
     }
 
     fn generateThumbnailAtlas(self: @This()) void {
-        const tex_folder = fs.path.join(aya.mem.allocator, &[_][]const u8{self.root_path, "textures"}) catch unreachable;
+        const tex_folder = fs.path.join(aya.mem.allocator, &[_][]const u8{ self.root_path, "textures" }) catch unreachable;
         defer aya.mem.allocator.free(tex_folder);
         const atlas = root.utils.texture_packer.packThumbnails(tex_folder, 50) catch unreachable;
         // const atlas = root.utils.texture_packer.pack(tex_folder) catch unreachable;
