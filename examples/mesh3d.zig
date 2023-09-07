91fe99c86973c133f4361d40cec05047b6ec6946
prime31 <mike@prime31.com>
2023-09-07 11:15:41 -0700

    fix fonts
    fix all examples (except tilemap)

--- examples/mesh3d.zig
+++ examples/mesh3d.zig
@@ -133,8 +133,8 @@ fn update() !void {
 }
 
 fn render() !void {
-    cube_rot_x += aya.time.dt();
-    cube_rot_y += aya.time.dt() * 2;
+    cube_rot_x += aya.math.toRadians(aya.time.dt());
+    cube_rot_y += aya.math.toRadians(aya.time.dt() * 2);
 
     const proj = Mat4.createPerspective(58, aya.window.aspectRatio(), 0.1, 10);
     const view = Mat4.createLookAt(Vec3.init(0, 1.5, 3), Vec3.init(0.0, 0.0, 0.0), Vec3.init(0.0, 1.0, 0.0));
