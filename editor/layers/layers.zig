f49927468579503efa14691ffb33adaf6e081140
prime31 <mike@prime31.com>
2023-09-08 16:20:03 -0700

    kill entire repo to start over

--- editor/layers/layers.zig
+++
@@ -1,5 +0,0 @@
-pub const Layer = @import("layer.zig").Layer;
-pub const LayerType = @import("layer.zig").LayerType;
-pub const TilemapLayer = @import("tilemap_layer.zig").TilemapLayer;
-pub const AutoTilemapLayer = @import("auto_tilemap_layer.zig").AutoTilemapLayer;
-pub const EntityLayer = @import("entity_layer.zig").EntityLayer;
--- editor/windows/windows.zig
+++
@@ -1,6 +0,0 @@
-pub const Scene = @import("scene.zig").Scene;
-
-pub const layers = @import("layers.zig");
-pub const component_editor = @import("component_editor.zig");
-pub const timeline = @import("timeline.zig");
-pub const assets = @import("assets.zig");
