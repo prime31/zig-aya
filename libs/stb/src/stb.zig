c9360c39c6d501696cfaff3151e3e8d96579ff37
prime31 <mike@prime31.com>
2023-09-12 13:36:22 -0700

    stbi added

---
+++ libs/stb/src/stb.zig
@@ -0,0 +1,48 @@
+const std = @import("std");
+
+pub usingnamespace @import("stb_image.zig");
+pub usingnamespace @import("stb_image_write.zig");
+pub usingnamespace @import("stb_rect_pack.zig");
+
+pub const Image = struct {
+    const stb = @import("stb_image.zig");
+
+    allocator: std.mem.Allocator = undefined,
+    stb_allocation: [*c]u8,
+    channels: u32,
+    w: u32,
+    h: u32,
+
+    pub fn init(allocator: std.mem.Allocator, path: []const u8) !Image {
+        const file = try std.fs.cwd().openFile(path, .{});
+        defer file.close();
+
+        const file_size = try file.getEndPos();
+        var buffer = try allocator.alloc(u8, file_size);
+        defer allocator.free(buffer);
+        _ = try file.read(buffer);
+
+        var w: c_int = undefined;
+        var h: c_int = undefined;
+        var channels: c_int = undefined;
+        const load_res = stb.stbi_load_from_memory(buffer.ptr, @as(c_int, @intCast(buffer.len)), &w, &h, &channels, 4);
+        if (load_res == null) return error.ImageLoadFailed;
+
+        defer stb.stbi_image_free(load_res);
+
+        return .{
+            .stb_allocation = load_res,
+            .channels = @intCast(channels),
+            .w = @intCast(w),
+            .h = @intCast(h),
+        };
+    }
+
+    pub fn deinit(self: *Image) void {
+        stb.stbi_image_free(self.stb_allocation);
+    }
+
+    pub fn getImageData(self: Image) []u8 {
+        return self.stb_allocation[0..@as(usize, @intCast(self.w * self.h * self.channels))];
+    }
+};
