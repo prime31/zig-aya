84c61ed5679a5886f8b6d618dce81fe350a30c3b
prime31 <mike@prime31.com>
2023-09-19 09:33:23 -0700

    image cleanup
    flex examples start

--- libs/flecs/build.zig
+++ libs/flecs/build.zig
@@ -1,9 +1,36 @@
 const std = @import("std");
 const Builder = std.build.Builder;
 
-// UPDATING FLECS
+// UPDATING FLECS (or use the `update_flecs` step)
 // - copy flecs.c and flecs.h
 // - zig translate-c flecs.h > flecs.zig
+
+pub fn build(b: *std.build.Builder) anyerror!void {
+    const target = b.standardTargetOptions(.{});
+    const optimize = b.standardOptimizeOption(.{});
+    const module = getModule(b);
+
+    const examples = getAllExamples(b, "examples");
+    for (examples) |example| {
+        const name = example[0];
+        const source = example[1];
+
+        const exe = b.addExecutable(.{
+            .name = name,
+            .root_source_file = .{ .path = source },
+            .target = target,
+            .optimize = optimize,
+        });
+        exe.addModule("ecs", module);
+        linkArtifact(b, exe, target, optimize);
+
+        const run_cmd = b.addRunArtifact(exe);
+        run_cmd.step.dependOn(b.getInstallStep());
+
+        const run_step = b.step(name, b.fmt("Run '{s}'", .{name}));
+        run_step.dependOn(&run_cmd.step);
+    }
+}
 
 pub fn linkArtifact(b: *std.build, exe: *std.Build.Step.Compile, target: std.zig.CrossTarget, optimize: std.builtin.Mode) void {
     exe.addIncludePath(.{ .path = thisDir() ++ "/lib" });
@@ -61,6 +88,36 @@ pub fn addFlecsUpdateStep(b: *std.build, target: std.zig.CrossTarget) void {
     }
 
     // TODO: write some sort of cleanup function if we end up updating flecs too often
+}
+
+fn getAllExamples(b: *std.build.Builder, root_directory: []const u8) [][2][]const u8 {
+    var list = std.ArrayList([2][]const u8).init(b.allocator);
+
+    const recursor = struct {
+        fn search(alloc: std.mem.Allocator, directory: []const u8, filelist: *std.ArrayList([2][]const u8)) void {
+            var dir = std.fs.cwd().openIterableDir(directory, .{ .access_sub_paths = true }) catch unreachable;
+            defer dir.close();
+
+            var iter = dir.iterate();
+            while (iter.next() catch unreachable) |entry| {
+                if (entry.kind == .file) {
+                    if (std.mem.endsWith(u8, entry.name, ".zig")) {
+                        const abs_path = std.fs.path.join(alloc, &[_][]const u8{ directory, entry.name }) catch unreachable;
+                        const name = std.fs.path.basename(abs_path);
+
+                        filelist.append([2][]const u8{ name[0 .. name.len - 4], abs_path }) catch unreachable;
+                    }
+                } else if (entry.kind == .directory) {
+                    const abs_path = std.fs.path.join(alloc, &[_][]const u8{ directory, entry.name }) catch unreachable;
+                    search(alloc, abs_path, filelist);
+                }
+            }
+        }
+    }.search;
+
+    recursor(b.allocator, root_directory, &list);
+
+    return list.toOwnedSlice() catch unreachable;
 }
 
 pub fn getModule(b: *std.Build) *std.build.Module {
---
+++ libs/flecs/examples/terms.zig
@@ -0,0 +1,7 @@
+const std = @import("std");
+const ecs = @import("ecs");
+const flecs = ecs.c;
+
+pub const Position = struct { x: f32, y: f32 };
+
+pub fn main() !void {}
