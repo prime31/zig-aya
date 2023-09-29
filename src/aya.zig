02377c2e58f8843e108bb9256229c0a9aed0922c
prime31 <mike@prime31.com>
2023-09-28 18:46:50 -0700

    start input

--- src/aya.zig
+++ src/aya.zig
@@ -5,8 +5,7 @@ pub const utils = @import("utils.zig");
 // inner modules
 pub usingnamespace @import("app/mod.zig");
 pub usingnamespace @import("asset/mod.zig");
-pub usingnamespace @import("input/mod.zig");
-pub usingnamespace @import("window/window.zig");
+pub usingnamespace @import("window/mod.zig");
 pub usingnamespace @import("ecs/mod.zig");
 
 // TODO: be more restrictive with exports and possibly dump them into sub-structs per module
