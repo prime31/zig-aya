04c582dc8a3f093bb1b6734710f37c70a4544e45
prime31 <mike@prime31.com>
2023-10-10 15:35:53 -0700

    clear color works

--- examples/gfx_clear.zig
+++ examples/gfx_clear.zig
@@ -2,11 +2,23 @@ const std = @import("std");
 const aya = @import("aya");
 
 const App = aya.App;
+const ResMut = aya.ResMut;
+const ClearColor = aya.ClearColor;
 
 pub fn main() !void {
     std.debug.print("\n", .{});
 
     App.init()
         .addPlugins(aya.DefaultPlugins)
+        .addSystems(aya.Update, ClearColorSystem)
         .run();
 }
+
+const ClearColorSystem = struct {
+    pub fn run(clear_color_res: ResMut(ClearColor)) void {
+        var clear_color = clear_color_res.getAssertExists();
+
+        clear_color.r += 0.005;
+        clear_color.r = if (clear_color.r > 1) 0 else clear_color.r;
+    }
+};
