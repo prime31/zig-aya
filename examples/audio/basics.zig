224aede6c7e49e85ca7bb9a378ee0890eddebb45
prime31 <mike@prime31.com>
2023-11-05 17:16:59 -0800

    audio

---
+++ examples/audio/basics.zig
@@ -0,0 +1,24 @@
+const std = @import("std");
+const aya = @import("aya");
+const ig = @import("imgui");
+
+pub fn main() !void {
+    std.debug.print("\n", .{});
+    try aya.run(.{
+        .init = init,
+        .render = render,
+    });
+}
+
+fn init() !void {
+    aya.audio.start();
+}
+
+fn render() !void {
+    if (aya.input.keyJustPressed(.a)) aya.audio.snd3.start() catch unreachable;
+    if (aya.input.keyJustPressed(.b)) aya.audio.snd1.start() catch unreachable;
+
+    aya.gfx.beginPass(.{});
+    aya.gfx.draw.rect(aya.math.Vec2.init(50, 50), 200, 400, aya.math.Color.lime);
+    aya.gfx.endPass();
+}
